2c74a1ce525a49b0465e8cf6f46f4248cfe0b348
Cherry <cherry@sparklet.org>
2024-07-04 16:31:30 -0400

    add manifest file stuff

--- lib/src/main/java/org/sparklet/firecat/FireCat.java
+++ lib/src/main/java/org/sparklet/firecat/FireCat.java
@@ -8,7 +8,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class zPbaa extends JavaPlugin {
+public class FireCat extends JavaPlugin {
 
     Player player;
 
